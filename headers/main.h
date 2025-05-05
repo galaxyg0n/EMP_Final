3c6342457d16d91a1fa11a419666f699ccb049e8
galaxyg0n <51724830+galaxyg0n@users.noreply.github.com>
2025-05-05 12:44:28 +0200

    Project setup

---
+++ headers/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 5 May 2025
+ *      Author: mathi
+ */
+
+#ifndef HEADERS_MAIN_H_
+#define HEADERS_MAIN_H_
+
+
+
+
+
+#endif /* HEADERS_MAIN_H_ */
---
+++ main.c
@@ -0,0 +1,14 @@
+
+
+/**
+ * main.c
+ *
+ */
+
+#include "main.h"
+#include "FreeRTOS.h"
+
+int main(void)
+{
+	return 0;
+}
